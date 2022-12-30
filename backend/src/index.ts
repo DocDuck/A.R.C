a77a14268a3ae9e4934c1c1f61625fb0a21f35e2
DocDuck <alex.mitin.psmu@gmail.com>
2022-12-30 12:04:37 +0500

    db setup
    добавил инициализацию дб через орм MikroORM, добавил 1 сущность Post на декораторах. Пока не взлетело

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
-import { MikroORM } from "@mikro-orm/core";
+import { MikroORM, defineConfig } from "@mikro-orm/core";
+import type { PostgreSqlDriver } from '@mikro-orm/postgresql';
+import { isProd } from "./utils/constants";
 
 const main = async () => {
-    const orm = await MikroORM.init();
+    const orm = await MikroORM.init<PostgreSqlDriver>({
+        dbName: 'ARC_DB',
+        type: "postgresql",
+        entities: ['./dist/entities'], // path to our JS entities (dist), relative to `baseDir`
+        entitiesTs: ['./src/entities'], // path to our TS entities (src), relative to `baseDir`
+        debug: !isProd       
+    });
+
+    console.log(orm.em)
 }
 
-
-
-console.log('hello world!!')
+main()
