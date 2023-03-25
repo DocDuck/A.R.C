327d9f6d19fccc600bab791ccc9553baf47157ee
DocDuck <alex.mitin.psmu@gmail.com>
2023-03-25 17:54:50 +0500

    express setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,25 @@
 import { MikroORM } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import { backendPort } from "./utils/constants";
 
 const main = async () => {
-    const orm = await MikroORM.init(mikroOrmConfig);
-    await orm.getMigrator().up();
-    const fork = orm.em.fork()
+    const app = express();
+    app.get('/', (_, res) => {
+        res.send('Preved medved')
+    })
+
+    app.listen(backendPort, () => {
+        console.log(`сервер запущен на порту ${backendPort}`)
+    })
+    // const orm = await MikroORM.init(mikroOrmConfig);
+    // await orm.getMigrator().up();
+    // const fork = orm.em.fork()
     // const post = fork.create(Post, {title: "Another post!"})
     // await fork.persistAndFlush(post);
-    const posts = await fork.find(Post,  {})
-    console.log(posts)
+    // const posts = await fork.find(Post,  {})
+    // console.log(posts)
 }
 
 main().catch(er => {
--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,4 +1,5 @@
 export const isProd = process.env.NODE_ENV === 'production'
+export const backendPort = 4000
 export const dbPort = 32770
 export const dbName = 'postgres'
 export const dbPassword = 'postgrespw'
